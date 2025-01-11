c8c2c9af5e0acbd4451d16d041611eca2acc6c2f
Swapnaneel Ray <neel26ray@gmail.com>
2025-01-11 17:10:09 +0530

    Blogs

--- app/blogs/page.js
+++ app/blogs/page.js
@@ -13,31 +13,35 @@ const Page = async () => {
         {posts.map((post, index) => (
           <div
             key={index}
-            className="max-w-sm rounded-md shadow-4xl shadow-blue-500 overflow-hidden shadow-lg m-4"
+            className="max-w-sm  h-[250px] w-[500px] rounded-md shadow-4xl shadow-blue-500 overflow-hidden shadow-lg m-4"
           >
             <div
               className="w-full p-5 bg-cover bg-center"
               style={{
-                // Use the URL directly for the background image
-                backgroundImage: post.image ? `url(/lifelabel.jpg)` : "none", // Fallback if image is missing
+                backgroundImage:
+                  post.category == "Tech"
+                    ? `url(/lifelabel.jpg)`
+                    : post.category == "Interview"
+                      ? `url(/interviewlabel.jpg)`
+                      : "none",
               }}
             >
-              <div className="bg-black bg-opacity-40 w-full h-full flex flex-col justify-between">
-                <div className="px-6 py-4 text-white">
+              <div className="bg-black bg-opacity-40 w-[350px] h-[220px] flex flex-col justify-between">
+                <div className="px-6 py-2 text-white">
                   <h2 className="font-bold text-2xl mb-2">{post.title}</h2>
                   <p className="text-sm">{post.description}</p>
                 </div>
 
                 <div className="px-6 pt-4 pb-2 flex justify-between items-center text-white">
                   <span className="text-sm">{formatDate(post._createdAt)}</span>
-                  <span className="text-sm border p-2 font-semibold">
+                  <span className="text-sm p-2 font-semibold">
                     {post.views} views
                   </span>
                 </div>
 
                 <div className="px-6 py-4">
                   <Link href={`/blogs/${post.title.toLowerCase()}`}>
-                    <Button>Read More</Button>
+                    <Button>Explore</Button>
                   </Link>
                 </div>
               </div>
